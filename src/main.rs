8dd4faca9b4aef5b8f90978a022bf400dae7fd0c
riceicetea <104317939+riceicetea@users.noreply.github.com>
2022-08-23 15:57:24 +0200

    add multi-file support

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,31 @@ use std::{env, fs, path::Path, process::exit};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.is_empty() {
-        println!("Provide a file name, or pass -a to see info about used libraries");
+    if args.len() == 1
+        || args.contains(&String::from("-h"))
+        || args.contains(&String::from("--help"))
+    {
+        println!("Provide file name(s) or pass -a/--about to see info about used libraries");
         exit(1);
     }
-    if args.contains(&String::from("a")) {
+
+    if args.contains(&String::from("-a")) || args.contains(&String::from("--about")) {
         let tpty = include_str!("../thirdparty/THIRDPARTY");
         println!("{}", tpty);
         return;
     }
 
-    // TODO allow multi file
-    let file_path = Path::new(args.get(1).unwrap())
-        .canonicalize()
-        .expect("Couldn't deref path");
-    // TODO: allow setting custom obsidian.json path with an arg
+    let mut files = vec![];
+
+    for i in 1..args.len() {
+        let file_path = Path::new(args.get(i).unwrap())
+            .canonicalize()
+            .expect("Couldn't deref path");
+        files.push(file_path);
+    }
 
     let config_dir = env::var("XDG_CONFIG_HOME").expect(
-        "Config dir not found at XDG_CONFIG_HOME. Please set a custom path to your obsidian.json",
+        "Config dir not found at XDG_CONFIG_HOME. Please set a custom path to your obsidian.json with -p/--path",
     );
 
     let obs_json_path = format!("{}/obsidian/obsidian.json", config_dir);
@@ -28,39 +35,29 @@ fn main() {
     let obs_json_parsed = json::parse(obsidian_json_contents.as_str())
         .expect(format!("Couldn't parse {}", obs_json_path).as_str());
 
-    let mut open_in_obsidian = false;
-
     let vaults = obs_json_parsed["vaults"].entries();
 
     for f in vaults {
         let th_path = &f.1["path"];
-        if file_path.starts_with(
-            th_path
-                .as_str()
-                .expect("Could not get vault path as string"),
-        ) {
-            open_in_obsidian = true;
-            break;
+        for file in &files {
+            if file.starts_with(
+                th_path
+                    .as_str()
+                    .expect("Could not get vault path as string"),
+            ) {
+                let urlencoded = urlencoding::encode(
+                    file.to_str()
+                        .expect("Couldn't convert found file path to str"),
+                )
+                .to_string();
+                let new_path = format!("obsidian://open?path={}", urlencoded.as_str());
+                open::that(new_path.clone())
+                    .expect(format!("Could not open obsidian link {}", new_path).as_str());
+                break;
+            } else {
+                open::that(file.to_str().expect("Couldn't convert found path to str"))
+                    .expect("Could not open using text editor");
+            }
         }
-    }
-
-    if open_in_obsidian {
-        let urlencoded = urlencoding::encode(
-            file_path
-                .to_str()
-                .expect("Couldn't convert found file path to str"),
-        )
-        .to_string();
-        let new_path = format!("obsidian://open?path={}", urlencoded.as_str());
-        open::that(new_path.clone())
-            .expect(format!("Could not open obsidian link {}", new_path).as_str());
-        return;
-    } else {
-        open::that(
-            file_path
-                .to_str()
-                .expect("Couldn't convert found path to str"),
-        )
-        .expect("Could not open using text editor");
     }
 }
