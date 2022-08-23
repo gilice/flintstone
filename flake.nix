9597b8d57f6ad516ccb00f334a955ffae4e967e1
riceicetea <104317939+riceicetea@users.noreply.github.com>
2022-08-23 15:17:44 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs {
+        inherit system;
+      };
+      in
+      {
+        defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "flintstone";
+          version = "0.1";
+          src = self;
+
+          nativeBuildInputs = with pkgs; [
+
+            # basic
+            rustc
+            cargo
+          ];
+
+          cargoSha256 = "sha256-BiHDmXNR/kMnkqQ3NJ+jwC1zFGtbW/vtlgZr54eDeAU=";
+        };
+
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            git config core.hooksPath .githooks
+          '';
+          buildInputs = with pkgs;
+            [
+              # basic
+              rustc
+              cargo
+
+              # for development
+              rustfmt
+
+              # custom
+              cargo-about
+              upx
+
+              # needed for SSL
+              openssl
+              pkg-config
+
+              git
+            ];
+
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+
+
+      }
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::{env, fs, path::Path};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // TODO allow multi file
+    let file_path = Path::new(args.get(1).expect("No file path provided"))
+        .canonicalize()
+        .expect("Couldn't deref path");
+    // TODO: allow setting custom obsidian.json path with an arg
+
+    let config_dir = env::var("XDG_CONFIG_HOME").expect(
+        "Config dir not found at XDG_CONFIG_HOME. Please set a custom path to your obsidian.json",
+    );
+
+    let obs_json_path = format!("{}/obsidian/obsidian.json", config_dir);
+    let obsidian_json_contents = fs::read_to_string(obs_json_path.as_str())
+        .expect(format!("Couldn't read {}", obs_json_path).as_str());
+    let obs_json_parsed = json::parse(obsidian_json_contents.as_str())
+        .expect(format!("Couldn't parse {}", obs_json_path).as_str());
+
+    let mut open_in_obsidian = false;
+
+    let vaults = obs_json_parsed["vaults"].entries();
+
+    for f in vaults {
+        let th_path = &f.1["path"];
+        if file_path.starts_with(
+            th_path
+                .as_str()
+                .expect("Could not get vault path as string"),
+        ) {
+            open_in_obsidian = true;
+            break;
+        }
+    }
+
+    if open_in_obsidian {
+        let urlencoded = urlencoding::encode(
+            file_path
+                .to_str()
+                .expect("Couldn't convert found file path to str"),
+        )
+        .to_string();
+        let new_path = format!("obsidian://open?path={}", urlencoded.as_str());
+        open::that(new_path.clone())
+            .expect(format!("Could not open obsidian link {}", new_path).as_str());
+        return;
+    } else {
+        open::that(
+            file_path
+                .to_str()
+                .expect("Couldn't convert found path to str"),
+        )
+        .expect("Could not open using text editor");
+    }
+}
